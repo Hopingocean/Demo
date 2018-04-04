081cec03811407dbe8aeb9fab8665586e6c0404c
Hopingocean <you@example.com>
2018-04-04 17:29:03 +0800

    2018/04/04 vue-multipage

---
+++ vue-multipage/src/views/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- <img src="../../assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <a href="test.html">跳转test页面</a>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-multipage/src/views/test/test.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import test from './test.vue'
+
+new Vue({
+    el: '#app',
+    template: '<test/>',
+    components: { test }
+})
