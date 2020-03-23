dc1d7f288da5b6b328e3f040ff4315618f7dea7a
Lee12589 <1258963839@qq.com>
2020-03-23 17:30:59 +0800

    socket.io client

--- socket-demo/index.js
+++ socket-demo/index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+  pingInterval: 10000,
+  pingTimeout: 25000
+});
 
 app.get('/', function(req, res) {
   // res.send('<h1>Hello world</h1>');
@@ -12,9 +15,27 @@ io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
+  socket.on('message', function(msg) {
+    console.log(msg);
   });
+  socket.on('disconnect', function(msg) {
+    console.log('disconnect:' + msg);
+  });
+  socket.on('error', function(error) {
+    console.log('error:' + error);
+  });
+  socket.on('disconnecting', function(reason) {
+    console.log('disconnecting:' + reason);
+  });
+  // socket.on('reconnect', function(msg) {
+  //   console.log('user reconnect' + msg);
+  // });
+  // socket.on('connect_error', function() {
+  //   console.log('user connect_error');
+  // });
+  // socket.on('connect_timeout', function() {
+  //   console.log('user connect_timeout');
+  // });
 });
 
 http.listen(3000, function() {
