1644221443fdf31170c895d915bda0b82b19f8e3
hopingocean <1258963839@qq.com>
2022-02-24 20:03:16 +0800

    template

---
+++ vue-3.0/src/views/components/ComTem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tem" ref="root">This is a root element</div>
+</template>
+
+<script>
+import { ref, onMounted, h } from 'vue';
+export default {
+  name: 'ComTem',
+  setup() {
+    // 模板引用
+    const root = ref(null);
+    onMounted(() => {
+      // Dom元素将在初始选然后分配给ref
+      console.log(root.value);
+    })
+    // return { root };
+
+    // return () => h('div', {
+    //   ref: root
+    // })
+
+    // JSX使用
+    return () => <div ref={root}></div>
+  }
+}
+</script>
